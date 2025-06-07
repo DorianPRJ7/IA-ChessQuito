9a863a675b947a17676f0e5d1bf7f347445cac54
Dorian_Pareja <dorian.pareja9@etu.univ-lorraine.fr>
2025-06-07 17:40:42 +0200

    Traduction des positions (A3, B4) en indices

--- ChessQuitto/chessQuitto_alpha_beta.py
+++ ChessQuitto/chessQuitto_alpha_beta.py
@@ -6,6 +6,14 @@ def nouveau_jeu():
         ['.', '.', '.', '.']
     ]
 
+
+def traduire_position(colonne, ligne):
+    # Colonne B Ligne 3 -> Indice 1 2,
+    indice_col = Colonnes.index(colonne)
+    indice_lig = Lignes.index(ligne)
+    return indice_col, indice_lig
+
+
 def afficher(jeu):
     print("  A B C D")
     print("1", jeu[0][0], jeu[0][1], jeu[0][2], jeu[0][3])
@@ -13,8 +21,12 @@ def afficher(jeu):
     print("3", jeu[2][0], jeu[2][1], jeu[2][2], jeu[2][3])
     print("4", jeu[3][0], jeu[3][1], jeu[3][2], jeu[3][3])
 
+
 def jouer():
     jeu = nouveau_jeu()
     afficher(jeu)
 
+
+Colonnes = ['A', 'B', 'C', 'D']
+Lignes = ['1', '2', '3', '4']
 jouer()
